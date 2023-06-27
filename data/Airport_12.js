d2f9d1b626128cfc1461eb9dfb4a7d86db471100
Nepotist <128513718+Nepotist@users.noreply.github.com>
2023-06-27 18:31:19 +0700

    Add files via upload

---
+++ data/Airport_12.js
@@ -0,0 +1,9 @@
+var json_Airport_12 = {
+"type": "FeatureCollection",
+"name": "Airport_12",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "name": "Международный аэропорт Новокузнецк имени Б.В. Волынова", "Фото": "<img src=\"images/аэронов.jpg\" align=”middle” width='250' height='150'>" }, "geometry": { "type": "Point", "coordinates": [ 86.863401591701262, 53.805723860456403 ] } },
+{ "type": "Feature", "properties": { "name": "Международный аэропорт Кемерово им. А. А. Леонова", "Фото": "<img src=\"images/аэрокем.jpg\" align=”middle” width='250' height='150'>" }, "geometry": { "type": "Point", "coordinates": [ 86.117287842177845, 55.281154114034329 ] } }
+]
+}
